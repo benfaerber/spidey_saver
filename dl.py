3b1c47d8b64161bda5cd8522d0d332e562f24f9f
Ben Faerber <benfaerber@protonmail.com>
2024-01-31 21:35:03 -0700

    Commit

--- dl.py
+++ dl.py
@@ -1,16 +1,29 @@
 from TikTokApi import TikTokApi
 import asyncio
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+SPIDEY_MAIN = "shearingshedvlogs" 
+SPIDEY_BACKUP = "turan.spidey"
+SPIDEY_ALTS = [
+    "spideywilliam",
+    "spideyturan",
+    "shearingshed",
+] 
 
 ms_token = os.environ.get(
-    "ms_token", None
-)  # set your own ms_token, think it might need to have visited a profile
+    "MS_TOKEN_A", None
+)
+
+if not ms_token:
+    raise Exception("No MS token")
 
 
 async def user_example():
     async with TikTokApi() as api:
         await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
-        user = api.user("therock")
+        user = api.user(SPIDEY_MAIN)
         user_data = await user.info()
         print(user_data)
 
